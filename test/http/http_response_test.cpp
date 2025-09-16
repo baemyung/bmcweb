03389183ab72f6b55faf2f8c7ef7aae59d5d29af
Myung Bae <myungbae@us.ibm.com>
2025-09-16 16:53:31 -0500

    Add misc-include-cleaner
    Add misc-include-cleaner
    
    Change-Id: Id3d2330bf5483951ef7bba425c46a9d666efdee0
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- test/http/http_response_test.cpp
+++ test/http/http_response_test.cpp
@@ -8,8 +8,8 @@
 #include <boost/beast/core/buffers_to_string.hpp>
 #include <boost/beast/core/file_base.hpp>
 #include <boost/beast/core/file_posix.hpp>
-#include <boost/beast/http/message.hpp>
-#include <boost/beast/http/serializer.hpp>
+#include <boost/beast/http/message_fwd.hpp>
+#include <boost/beast/http/serializer_fwd.hpp>
 #include <boost/beast/http/status.hpp>
 
 #include <cstdio>
