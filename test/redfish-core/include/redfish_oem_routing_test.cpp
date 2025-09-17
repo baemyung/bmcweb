69a2790e864fd86678275dd930d7c3b17a0f650a
Myung Bae <myungbae@us.ibm.com>
2025-09-17 07:27:04 -0500

    Add Assembly Route test
    Add Assembly Head/Get routes
    
    Change-Id: Ia646e8c8022dcbc44fa06582ef440764f457972c
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- test/redfish-core/include/redfish_oem_routing_test.cpp
+++ test/redfish-core/include/redfish_oem_routing_test.cpp
@@ -2,6 +2,7 @@
 #include "async_resp.hpp"
 #include "http_request.hpp"
 #include "redfish.hpp"
+#include "registries/privilege_registry.hpp"
 #include "sub_request.hpp"
 #include "verb.hpp"
 
@@ -20,6 +21,108 @@ namespace redfish
 {
 namespace
 {
+
+TEST(OemRouter, FragmentRoutesWithAssembly)
+{
+    std::error_code ec;
+    App app;
+    RedfishService service(app);
+
+    bool assemblyHeadCalled = false;
+    auto assemblyHeadCallback =
+        [&assemblyHeadCalled](const crow::Request&,
+                              const std::shared_ptr<bmcweb::AsyncResp>&,
+                              const std::string& chassisId) {
+            assemblyHeadCalled = true;
+            EXPECT_EQ(chassisId, "chassis");
+        };
+
+    bool assemblyGetCalled = false;
+    auto assemblyGetCallback =
+        [&assemblyGetCalled](const crow::Request&,
+                             const std::shared_ptr<bmcweb::AsyncResp>&,
+                             const std::string& chassisId) {
+            assemblyGetCalled = true;
+            EXPECT_EQ(chassisId, "chassis");
+        };
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Chassis/<str>/Assembly")
+        .privileges(redfish::privileges::headAssembly)
+        .methods(boost::beast::http::verb::head)(assemblyHeadCallback);
+
+    BMCWEB_ROUTE(app, "/redfish/v1/Chassis/<str>/Assembly")
+        .privileges(redfish::privileges::getAssembly)
+        .methods(boost::beast::http::verb::get)(assemblyGetCallback);
+
+    // Need the normal route registered for OEM to work
+
+    // Callback handler that does nothing
+    bool oemCalled = false;
+    auto oemCallback = [&oemCalled](const SubRequest&,
+                                    const std::shared_ptr<bmcweb::AsyncResp>&,
+                                    const std::string& bar) {
+        oemCalled = true;
+        EXPECT_EQ(bar, "bar");
+    };
+
+    bool standardCalled = false;
+    auto standardCallback =
+        [&standardCalled,
+         &service](const crow::Request& req,
+                   const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+                   const std::string& bar) {
+            service.handleSubRoute(req, asyncResp);
+            standardCalled = true;
+            EXPECT_EQ(bar, "bar");
+        };
+
+    BMCWEB_ROUTE(app, "/foo/<str>/")
+        .privileges(redfish::privileges::headAssembly)
+        .methods(boost::beast::http::verb::head)(standardCallback);
+
+    BMCWEB_ROUTE(app, "/foo/<str>/")
+        .privileges(redfish::privileges::getAssembly)
+        .methods(boost::beast::http::verb::get)(standardCallback);
+
+    REDFISH_SUB_ROUTE<"/foo/<str>/#/Oem">(service, HttpVerb::Get)(oemCallback);
+
+    app.validate();
+    service.validate();
+
+    {
+        //
+        std::shared_ptr<bmcweb::AsyncResp> asyncResp =
+            std::make_shared<bmcweb::AsyncResp>();
+
+        // foo bar
+        constexpr std::string_view reqUrl = "/foo/bar";
+        std::shared_ptr<crow::Request> req = std::make_shared<crow::Request>(
+            crow::Request::Body{boost::beast::http::verb::get, reqUrl, 11}, ec);
+
+        // assembly
+        constexpr std::string_view reqAssemblyUrl =
+            "/redfish/v1/Chassis/chassis/Assembly";
+        std::shared_ptr<crow::Request> reqHeadAssembly =
+            std::make_shared<crow::Request>(
+                crow::Request::Body{boost::beast::http::verb::head,
+                                    reqAssemblyUrl, 11},
+                ec);
+        std::shared_ptr<crow::Request> reqGetAssembly =
+            std::make_shared<crow::Request>(
+                crow::Request::Body{boost::beast::http::verb::get,
+                                    reqAssemblyUrl, 11},
+                ec);
+
+        // perform handle
+        app.handle(req, asyncResp);
+        app.handle(reqHeadAssembly, asyncResp);
+        app.handle(reqGetAssembly, asyncResp);
+    }
+    EXPECT_TRUE(oemCalled);
+    EXPECT_TRUE(standardCalled);
+    EXPECT_TRUE(assemblyHeadCalled);
+    EXPECT_TRUE(assemblyGetCalled);
+}
 
 TEST(OemRouter, FragmentRoutes)
 {
