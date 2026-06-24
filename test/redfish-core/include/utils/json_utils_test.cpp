9ca9d1838e3089b5feca91549a27bbfda157430f
Myung Bae <myungbae@us.ibm.com>
2026-06-24 17:55:42 -0500

    Add debug on test
    Add debug statement on CI unit test
    
    Change-Id: Id91a6ba9ede4c86bd48524d4146655223389638e
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- test/redfish-core/include/utils/json_utils_test.cpp
+++ test/redfish-core/include/utils/json_utils_test.cpp
@@ -10,7 +10,7 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <functional>
+#include <iostream>
 #include <optional>
 #include <string>
 #include <system_error>
@@ -408,276 +408,23 @@ TEST(ReadJsonPatch, VerifyReadJsonPatchBadVectorObject)
     ASSERT_FALSE(readJson(jsonRequest, res, "NotVector", indices));
     EXPECT_EQ(res.result(), boost::beast::http::status::bad_request);
 
+    std::cout << " XXX.VerifyReadJsonPatchBadVectorObject222="
+              << res.jsonValue.dump(-1, ' ', true,
+                                    nlohmann::json::error_handler_t::replace)
+              << "\n";
+
     const nlohmann::json& argsExtInfo =
         res.jsonValue["NotVector@Message.ExtendedInfo"][0];
+
+    // EXPECT_THAT(argsExtInfo, "XXXX");
+
+    std::cout << " XXX.argsExtInfo4444="
+              << argsExtInfo.dump(-1, ' ', true,
+                                  nlohmann::json::error_handler_t::replace)
+              << "\n";
+
     EXPECT_THAT(argsExtInfo["MessageArgs"][0], "1");
     EXPECT_THAT(argsExtInfo["MessageArgs"][1], "NotVector");
-}
-
-TEST(ReadJsonAction, ValidElementsReturnsTrueResponseOkValuesUnpackedCorrectly)
-{
-    crow::Response res;
-    std::error_code ec;
-    crow::Request req("{\"integer\": 1}", ec);
-    req.addHeader(boost::beast::http::field::content_type, "application/json");
-    // Ignore errors intentionally
-
-    int64_t integer = 0;
-    ASSERT_TRUE(readJsonAction(req, res, "integer", integer));
-    EXPECT_EQ(res.result(), boost::beast::http::status::ok);
-    EXPECT_THAT(res.jsonValue, IsEmpty());
-    EXPECT_EQ(integer, 1);
-}
-
-TEST(ReadJsonAction, EmptyObjectReturnsTrueResponseOk)
-{
-    crow::Response res;
-    std::error_code ec;
-    crow::Request req({"{}"}, ec);
-    req.addHeader(boost::beast::http::field::content_type, "application/json");
-    // Ignore errors intentionally
-
-    std::optional<int64_t> integer = 0;
-    ASSERT_TRUE(readJsonAction(req, res, "integer", integer));
-    EXPECT_EQ(res.result(), boost::beast::http::status::ok);
-    EXPECT_THAT(res.jsonValue, IsEmpty());
-}
-
-TEST(odataObjectCmp, PositiveCases)
-{
-    EXPECT_EQ(0, odataObjectCmp(R"({"@odata.id": "/redfish/v1/1"})"_json,
-                                R"({"@odata.id": "/redfish/v1/1"})"_json));
-    EXPECT_EQ(0, odataObjectCmp(R"({"@odata.id": ""})"_json,
-                                R"({"@odata.id": ""})"_json));
-    EXPECT_EQ(0, odataObjectCmp(R"({"@odata.id": 42})"_json,
-                                R"({"@odata.id": 0})"_json));
-    EXPECT_EQ(0, odataObjectCmp(R"({})"_json, R"({})"_json));
-
-    EXPECT_GT(0, odataObjectCmp(R"({"@odata.id": "/redfish/v1"})"_json,
-                                R"({"@odata.id": "/redfish/v1/1"})"_json));
-    EXPECT_LT(0, odataObjectCmp(R"({"@odata.id": "/redfish/v1/1"})"_json,
-                                R"({"@odata.id": "/redfish/v1"})"_json));
-
-    EXPECT_LT(0, odataObjectCmp(R"({"@odata.id": "/10"})"_json,
-                                R"({"@odata.id": "/1"})"_json));
-    EXPECT_GT(0, odataObjectCmp(R"({"@odata.id": "/1"})"_json,
-                                R"({"@odata.id": "/10"})"_json));
-
-    EXPECT_GT(0, odataObjectCmp(R"({})"_json, R"({"@odata.id": "/1"})"_json));
-    EXPECT_LT(0, odataObjectCmp(R"({"@odata.id": "/1"})"_json, R"({})"_json));
-
-    EXPECT_GT(0, odataObjectCmp(R"({"@odata.id": 4})"_json,
-                                R"({"@odata.id": "/1"})"_json));
-    EXPECT_LT(0, odataObjectCmp(R"({"@odata.id": "/1"})"_json,
-                                R"({"@odata.id": 4})"_json));
-}
-
-TEST(SortJsonArrayByOData, ElementMissingKeyReturnsFalseArrayIsPartlySorted)
-{
-    nlohmann::json::array_t array;
-    array.push_back(R"({"@odata.id" : "/redfish/v1/100"})"_json);
-    array.push_back(R"({"@odata.id" : "/redfish/v1/1"})"_json);
-    array.push_back(R"({"@odata.id" : "/redfish/v1/20"})"_json);
-
-    sortJsonArrayByOData(array);
-    // Objects with other keys are always larger than those with the specified
-    // key.
-    EXPECT_THAT(array,
-                ElementsAre(R"({"@odata.id": "/redfish/v1/1"})"_json,
-                            R"({"@odata.id" : "/redfish/v1/20"})"_json,
-                            R"({"@odata.id" : "/redfish/v1/100"})"_json));
-}
-
-TEST(SortJsonArrayByOData, SortedByStringValueOnSuccessArrayIsSorted)
-{
-    nlohmann::json::array_t array;
-    array.push_back(R"({"@odata.id" : "/redfish/v1/20"})"_json);
-    array.push_back(R"({"@odata.id" : "/redfish/v1"})"_json);
-    array.push_back(R"({"@odata.id" : "/redfish/v1/100"})"_json);
-
-    sortJsonArrayByOData(array);
-    EXPECT_THAT(array,
-                ElementsAre(R"({"@odata.id": "/redfish/v1"})"_json,
-                            R"({"@odata.id" : "/redfish/v1/20"})"_json,
-                            R"({"@odata.id" : "/redfish/v1/100"})"_json));
-}
-
-TEST(objectKeyCmp, PositiveCases)
-{
-    EXPECT_EQ(
-        0, objectKeyCmp("@odata.id",
-                        R"({"@odata.id": "/redfish/v1/1", "Name": "a"})"_json,
-                        R"({"@odata.id": "/redfish/v1/1", "Name": "b"})"_json));
-    EXPECT_GT(
-        0, objectKeyCmp("Name",
-                        R"({"@odata.id": "/redfish/v1/1", "Name": "a"})"_json,
-                        R"({"@odata.id": "/redfish/v1/1", "Name": "b"})"_json));
-    EXPECT_EQ(0, objectKeyCmp(
-                     "Name",
-                     R"({"@odata.id": "/redfish/v1/1", "Name": "a 45"})"_json,
-                     R"({"@odata.id": "/redfish/v1/1", "Name": "a 45"})"_json));
-    EXPECT_GT(0, objectKeyCmp(
-                     "Name",
-                     R"({"@odata.id": "/redfish/v1/1", "Name": "a 45"})"_json,
-                     R"({"@odata.id": "/redfish/v1/1", "Name": "b 45"})"_json));
-
-    EXPECT_GT(
-        0, objectKeyCmp("@odata.id",
-                        R"({"@odata.id": "/redfish/v1/1", "Name": "b"})"_json,
-                        R"({"@odata.id": "/redfish/v1/2", "Name": "b"})"_json));
-    EXPECT_EQ(
-        0, objectKeyCmp("Name",
-                        R"({"@odata.id": "/redfish/v1/1", "Name": "b"})"_json,
-                        R"({"@odata.id": "/redfish/v1/2", "Name": "b"})"_json));
-
-    EXPECT_LT(0,
-              objectKeyCmp(
-                  "@odata.id",
-                  R"({"@odata.id": "/redfish/v1/p10/", "Name": "a1"})"_json,
-                  R"({"@odata.id": "/redfish/v1/p1/", "Name": "a10"})"_json));
-    EXPECT_GT(0,
-              objectKeyCmp(
-                  "Name",
-                  R"({"@odata.id": "/redfish/v1/p10/", "Name": "a1"})"_json,
-                  R"({"@odata.id": "/redfish/v1/p1/", "Name": "a10"})"_json));
-
-    nlohmann::json leftRequest =
-        R"({"Name": "fan1", "@odata.id": "/redfish/v1/Chassis/chassis2"})"_json;
-    nlohmann::json rightRequest =
-        R"({"Name": "fan2", "@odata.id": "/redfish/v1/Chassis/chassis1"})"_json;
-
-    EXPECT_GT(0, objectKeyCmp("Name", leftRequest, rightRequest));
-    EXPECT_LT(0, objectKeyCmp("@odata.id", leftRequest, rightRequest));
-    EXPECT_EQ(0, objectKeyCmp("DataSourceUri", leftRequest, rightRequest));
-}
-
-TEST(SortJsonArrayByKey, ElementMissingKeyReturnsFalseArrayIsPartlySorted)
-{
-    nlohmann::json::array_t array;
-    array.push_back(R"({"@odata.id" : "/redfish/v1/100"})"_json);
-    array.push_back(R"({"Name": "/redfish/v1/5"})"_json);
-    array.push_back(R"({"@odata.id" : "/redfish/v1/1"})"_json);
-    array.push_back(R"({"@odata.id" : "/redfish/v1/20"})"_json);
-    sortJsonArrayByKey(array, "@odata.id");
-    // Objects with other keys are always smaller than those with the specified
-    // key.
-    EXPECT_THAT(array,
-                ElementsAre(R"({"Name" : "/redfish/v1/5"})"_json,
-                            R"({"@odata.id": "/redfish/v1/1"})"_json,
-                            R"({"@odata.id" : "/redfish/v1/20"})"_json,
-                            R"({"@odata.id" : "/redfish/v1/100"})"_json));
-}
-
-TEST(SortJsonArrayByKey, SortedByStringValueOnSuccessArrayIsSorted)
-{
-    nlohmann::json::array_t array;
-    array.push_back(R"({"@odata.id" : "/redfish/v1/20", "Name": "a"})"_json);
-    array.push_back(R"({"@odata.id" : "/redfish/v1", "Name": "c"})"_json);
-    array.push_back(R"({"@odata.id" : "/redfish/v1/100", "Name": "b"})"_json);
-
-    sortJsonArrayByKey(array, "@odata.id");
-    EXPECT_THAT(
-        array,
-        ElementsAre(R"({"@odata.id": "/redfish/v1", "Name": "c"})"_json,
-                    R"({"@odata.id": "/redfish/v1/20", "Name": "a"})"_json,
-                    R"({"@odata.id": "/redfish/v1/100", "Name": "b"})"_json));
-
-    sortJsonArrayByKey(array, "Name");
-    EXPECT_THAT(
-        array,
-        ElementsAre(R"({"@odata.id": "/redfish/v1/20", "Name": "a"})"_json,
-                    R"({"@odata.id": "/redfish/v1/100", "Name": "b"})"_json,
-                    R"({"@odata.id": "/redfish/v1", "Name": "c"})"_json));
-}
-
-TEST(GetEstimatedJsonSize, NumberIs8Bytpes)
-{
-    EXPECT_EQ(getEstimatedJsonSize(nlohmann::json(123)), 8);
-    EXPECT_EQ(getEstimatedJsonSize(nlohmann::json(77777777777)), 8);
-    EXPECT_EQ(getEstimatedJsonSize(nlohmann::json(3.14)), 8);
-}
-
-TEST(GetEstimatedJsonSize, BooleanIs5Byte)
-{
-    EXPECT_EQ(getEstimatedJsonSize(nlohmann::json(true)), 5);
-    EXPECT_EQ(getEstimatedJsonSize(nlohmann::json(false)), 5);
-}
-
-TEST(GetEstimatedJsonSize, NullIs4Byte)
-{
-    EXPECT_EQ(getEstimatedJsonSize(nlohmann::json()), 4);
-}
-
-TEST(GetEstimatedJsonSize, StringAndBytesReturnsLengthAndQuote)
-{
-    EXPECT_EQ(getEstimatedJsonSize(nlohmann::json("1234")), 6);
-    EXPECT_EQ(getEstimatedJsonSize(nlohmann::json::binary({1, 2, 3, 4})), 4);
-}
-
-TEST(GetEstimatedJsonSize, ArrayReturnsSum)
-{
-    nlohmann::json arr = {1, 3.14, "123", nlohmann::json::binary({1, 2, 3, 4})};
-    EXPECT_EQ(getEstimatedJsonSize(arr), 8 + 8 + 5 + 4);
-}
-
-TEST(GetEstimatedJsonSize, ObjectsReturnsSumWithKeyAndValue)
-{
-    nlohmann::json obj = R"(
-{
-  "key0": 123,
-  "key1": "123",
-  "key2": [1, 2, 3],
-  "key3": {"key4": "123"}
-}
-)"_json;
-
-    uint64_t expected = 0;
-    // 5 keys of length 4
-    expected += uint64_t(5) * 4;
-    // 5 colons, 5 quote pairs, and 5 spaces for 5 keys
-    expected += uint64_t(5) * (1 + 2 + 1);
-    // 2 string values of length 3
-    expected += uint64_t(2) * (3 + 2);
-    // 1 number value
-    expected += 8;
-    // 1 array value of 3 numbers
-    expected += uint64_t(3) * 8;
-    EXPECT_EQ(getEstimatedJsonSize(obj), expected);
-}
-
-TEST(hashJsonWithoutKey, HashObject)
-{
-    nlohmann::json obj = R"(
-{
-  "key0": 123,
-  "key1": "123",
-  "key2": [1, 2, 3],
-  "key3": {"key4": "123"}
-}
-)"_json;
-
-    // Returns same value as std::hash when no key is ignored
-    size_t originalHash = std::hash<nlohmann::json>{}(obj);
-    EXPECT_EQ(originalHash, hashJsonWithoutKey(obj, "other"));
-
-    nlohmann::json modifiedObj;
-    // ast-grep-ignore: bmcweb.common-errors.throwing-json-items
-    for (const auto& element : obj.items())
-    {
-        // Hash with ignored key is different from original hash
-        EXPECT_NE(originalHash, hashJsonWithoutKey(obj, element.key()));
-
-        // Hash with ignored key is different than just removing the key
-        modifiedObj = obj;
-        modifiedObj.erase(element.key());
-        EXPECT_NE(std::hash<nlohmann::json>{}(modifiedObj),
-                  hashJsonWithoutKey(obj, element.key()));
-    }
-
-    // Ignored key is not removed recursively
-    modifiedObj = obj;
-    modifiedObj["key3"].erase("key4");
-    EXPECT_EQ(originalHash, hashJsonWithoutKey(obj, "key4"));
 }
 
 } // namespace
