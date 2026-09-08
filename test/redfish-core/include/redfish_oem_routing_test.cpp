d51030629c44197a4de4f1a887b680cabeceac00
Myung Bae <myungbae@us.ibm.com>
2026-09-08 13:36:59 -0500

    Add OemFoo readJsonObject test
    Add OemFoo readJsonObject test
    
    Change-Id: I0c4c447b1346e62c385260f1aa260e48fc409afb
    Signed-off-by: Myung Bae <myungbae@us.ibm.com>

--- test/redfish-core/include/redfish_oem_routing_test.cpp
+++ test/redfish-core/include/redfish_oem_routing_test.cpp
@@ -1,14 +1,17 @@
 #include "app.hpp"
 #include "async_resp.hpp"
 #include "http_request.hpp"
+#include "http_response.hpp"
 #include "redfish.hpp"
 #include "sub_request.hpp"
+#include "utils/json_utils.hpp"
 #include "verb.hpp"
 
 #include <boost/beast/http/verb.hpp>
 #include <nlohmann/json.hpp>
 
 #include <memory>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <system_error>
@@ -85,7 +88,7 @@ TEST(OemRouter, PatchHandlerWithJsonObject)
             [[maybe_unused]] const std::string& param) {
             callback1Called = true;
 
-            const nlohmann::json::object_t& payload = req.payload();
+            nlohmann::json::object_t payload = req.payload();
             auto oemFooIt = payload.find("OemFoo");
             ASSERT_NE(oemFooIt, payload.end());
             ASSERT_TRUE(oemFooIt->second.is_object());
@@ -93,6 +96,11 @@ TEST(OemRouter, PatchHandlerWithJsonObject)
             auto keyIt = oemFooIt->second.find("OemFooKey");
             ASSERT_NE(keyIt, oemFooIt->second.end());
             EXPECT_EQ(keyIt.value(), "fooValue");
+
+            std::optional<std::string> fooValue;
+            ASSERT_TRUE(json_util::readJsonObject(
+                payload, asyncResp->res, "OemFoo/OemFooKey", fooValue));
+            EXPECT_EQ(fooValue, "fooValue");
         };
 
     bool callback2Called = false;
@@ -105,7 +113,7 @@ TEST(OemRouter, PatchHandlerWithJsonObject)
             [[maybe_unused]] const std::string& param) {
             callback2Called = true;
 
-            const nlohmann::json::object_t& payload = req.payload();
+            nlohmann::json::object_t payload = req.payload();
             auto oemBarIt = payload.find("OemBar");
             ASSERT_NE(oemBarIt, payload.end());
             ASSERT_TRUE(oemBarIt->second.is_object());
@@ -113,6 +121,11 @@ TEST(OemRouter, PatchHandlerWithJsonObject)
             auto keyIt = oemBarIt->second.find("OemBarKey");
             ASSERT_NE(keyIt, oemBarIt->second.end());
             EXPECT_EQ(keyIt.value(), "barValue");
+
+            std::optional<std::string> barValue;
+            ASSERT_TRUE(json_util::readJsonObject(
+                payload, asyncResp->res, "OemBar/OemBarKey", barValue));
+            EXPECT_EQ(barValue, "barValue");
         };
 
     bool standardCalled = false;
